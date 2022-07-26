b93bc378f0974817cb15a68f141929c20e67fc5b
Gaston Genaud <gastongenaudar@gmail.com>
2022-07-26 01:02:05 -0300

    add step functions and POM folder

---
+++ cypress/integration/steps/adminSuite-steps.js
@@ -0,0 +1,56 @@
+import { Given, Then, When, And } from "cypress-cucumber-preprocessor/steps";
+
+Given("I go to Myshop4 Page", () => {});
+
+// When section
+When("I locate the title of the page", () => {});
+
+When("I login with {} credentials", (arg) => {});
+
+// Then section
+Then("I validate that it is visible", () => {});
+
+Then("i validate the wrong login message", () => {});
+
+Then("i validate successfull login", () => {});
+
+Then("I validate add new category and edit it", () => {});
+
+Then("check that the Products quantity is the one expected.", () => {});
+
+Then("save data into a file {}", (arg) => {});
+
+// And section
+And("I validate that the title is {}", (arg) => {});
+
+And("select Categories section", () => {});
+
+And("click the Add button", () => {});
+
+And("write {}", (arg) => {});
+
+And("select an image", () => {});
+
+And("check Available checkbox", () => {});
+
+And("select button Add", () => {});
+
+And("edit the category {}", (arg) => {});
+
+And("select Update button", () => {});
+
+And("Add a product with price {}", (arg) => {});
+
+And("edit the product", () => {});
+
+And("Filter the products by the category {}", (arg) => {});
+
+And("I show the number of results", () => {});
+
+And("I show the number of results", () => {});
+
+And("Delete the product with price {}", (arg) => {});
+
+And("I go to My Dashboard", () => {});
+
+And("click on share button on {}", (arg) => {});
